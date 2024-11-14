a520a82df8a53a2539996d8d43745b800d089920
Solansra <solansra@gmail.com>
2024-11-14 18:35:45 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/dto/UserRegisterResult.java
+++ src/main/java/com/lmlasmo/genegisc/dto/UserRegisterResult.java
@@ -2,4 +2,26 @@ package com.lmlasmo.genegisc.dto;
 
 public class UserRegisterResult {
 
+	public boolean sucess;
+	
+	public UserDTO userDto;
+	
+	public UserRegisterResult() {}
+
+	public boolean isSucess() {
+		return sucess;
+	}
+
+	public void setSucess(boolean sucess) {
+		this.sucess = sucess;
+	}
+
+	public UserDTO getUserDto() {
+		return userDto;
+	}
+
+	public void setUserDto(UserDTO userDto) {
+		this.userDto = userDto;
+	}	
+	
 }
