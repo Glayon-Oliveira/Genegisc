d93f179d50c38b20aac12a30bbe4b802d6fae577
Solansra <solansra@gmail.com>
2024-11-14 17:00:30 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/model/User.java
+++ src/main/java/com/lmlasmo/genegisc/model/User.java
@@ -1,5 +1,72 @@
 package com.lmlasmo.genegisc.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "profile_name", nullable = false)
+	private String profileName;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(name = "birth_date")
+	private LocalDate birthDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}	
 
 }
