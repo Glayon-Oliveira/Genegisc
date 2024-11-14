989e3ba2b7aaff34f02baa3eab99d3e20e38b7b1
Solansra <solansra@gmail.com>
2024-11-14 17:07:46 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/dto/UserDTO.java
+++ src/main/java/com/lmlasmo/genegisc/dto/UserDTO.java
@@ -1,5 +1,42 @@
 package com.lmlasmo.genegisc.dto;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserDTO {
 
+	@JsonProperty("username")
+	private String username;
+	
+	@JsonProperty("profile_name")
+	private String profileName;
+	
+	@JsonProperty("birth_date")
+	private LocalDate birthDate;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}	
+	
 }
