93d1793a6128689dc942ecd7871db8ce7cc17753
Solansra <solansra@gmail.com>
2024-11-14 17:57:04 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/service/UserRegisterService.java
+++ src/main/java/com/lmlasmo/genegisc/service/UserRegisterService.java
@@ -1,5 +1,92 @@
 package com.lmlasmo.genegisc.service;
 
-public class UserRegisterService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lmlasmo.genegisc.dto.UserDTO;
+import com.lmlasmo.genegisc.model.User;
+import com.lmlasmo.genegisc.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserRegisterService {
+		
+	public UserRepository repository;	
+	
+	@Autowired
+	public UserRegisterService(UserRepository repository) {
+		this.repository = repository;
+	}
+	
+	public Optional<User> alterProfileName(User user, String profileName) {
+		
+		Optional<User> userOp = Optional.ofNullable(null);
+		
+		if(!user.getProfileName().equals(profileName)) {
+			
+			user.setProfileName(profileName);
+			
+			userOp = Optional.ofNullable(repository.save(user));
+			
+			if(userOp.isPresent()) {
+				
+				if(!userOp.get().getProfileName().equals(profileName)) {					
+					userOp = Optional.ofNullable(null);
+				}
+				
+			}						
+			
+		}
+		
+		return userOp;
+		
+	}
+	
+	public Optional<User> alterPassword(User user, String password){
+		
+		Optional<User> userOp = Optional.ofNullable(null);
+		
+		if(!user.getPassword().equals(password)) {
+			
+			user.setPassword(password);
+			
+			userOp = Optional.ofNullable(repository.save(user));
+			
+			if(userOp.isPresent()) {
+				
+				if(userOp.get().getPassword().equals(password)) {
+					userOp = Optional.ofNullable(null);
+				}
+				
+			}
+			
+		}
+		
+		return userOp;
+		
+	}
+	
+	public Optional<User> save(UserDTO dto, String password) {
+		
+		User user = new User();
+		
+		user.setUsername(dto.getUsername());
+		user.setProfileName(dto.getProfileName());
+		user.setBirthDate(dto.getBirthDate());		
+		
+		return Optional.ofNullable(repository.save(user));
+		
+	}
+	
+	public boolean delete(User user) {		
+		
+		repository.delete(user);
+		
+		return repository.findById(user.getId()).isEmpty();
+		
+	}
+	
 }
--- src/main/java/com/lmlasmo/genegisc/service/UserService.java
+++ src/main/java/com/lmlasmo/genegisc/service/UserService.java
@@ -1,5 +1,80 @@
 package com.lmlasmo.genegisc.service;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lmlasmo.genegisc.dto.UserDTO;
+import com.lmlasmo.genegisc.model.User;
+import com.lmlasmo.genegisc.repository.UserRepository;
+
+public class UserService extends UserRegisterService{
+	
+	@Autowired
+	public UserService(UserRepository repository) {
+		super(repository);
+	}
+	
+	public List<User> findAll(){
+		return this.repository.findAll();
+	}
+	
+	public Optional<User> findByUsername(String username){
+		return this.repository.findByUsername(username);
+	}
+	
+	public List<User> findByProfileNameContaining(String profileName){
+		return this.repository.findByProfileNameContaining(profileName);
+	}
+	
+	public List<User> findByBirthYear(int year){
+		return this.repository.findByBirthYear(year);
+	}
+	
+	public List<User> findByBirthMount(int mount){
+		return this.repository.findByBirthMount(mount);
+	}
+	
+	public List<User> findByBirthDateStartingFromYear(int year){
+		return this.repository.findByBirthDateStartingFromYear(year);
+	}
+	
+	public Optional<UserDTO> toDTO(User user) {
+		
+		Optional<UserDTO> dtoOp = Optional.ofNullable(null);
+		
+		if(verifyToDTO(user)) {
+			
+			dtoOp = Optional.ofNullable(new UserDTO(user));			
+			
+		}
+		
+		return dtoOp;
+		
+	}
+	
+	public List<UserDTO> toDTO(List<User> users) {
+		
+		List<UserDTO> dtoList = users.stream()
+									 .filter(u -> verifyToDTO(u))
+									 .map(u -> toDTO(u))
+									 .filter(u -> u.isPresent())
+									 .map(u -> u.get())
+									 .collect(Collectors.toList());
+		
+		return dtoList;
+		
+	}
+	
+	private boolean verifyToDTO(User user) {
+		
+		boolean cond = (user.getUsername() != null && user.getProfileName() != null && user.getPassword() != null);
+		
+		return cond;
+		
+	}
+	
 
 }
