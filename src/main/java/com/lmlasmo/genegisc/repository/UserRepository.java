6604f1d947b50d6f8fad17466e6fb3bf918b2b72
Solansra <solansra@gmail.com>
2024-11-14 17:31:10 -0300

    Defined methods

--- src/main/java/com/lmlasmo/genegisc/repository/UserRepository.java
+++ src/main/java/com/lmlasmo/genegisc/repository/UserRepository.java
@@ -1,5 +1,27 @@
 package com.lmlasmo.genegisc.repository;
 
-public interface UserRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.lmlasmo.genegisc.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	public List<User> findByProfileNameContaining(String profileName);
+	
+	public Optional<User> findByUsername(String username);
+	
+	@Query("SELECT u FROM User u WHERE YEAR(u.birthDate) = :year")
+	public List<User> findByBirthYear(@Param("year") int year);
+	
+	@Query("SELECT u FROM User u WHERE MOUNT(u.birthDate) = :mount")
+	public List<User> findByBirthMount(@Param("mount") int mount);
+	
+	@Query("SELECT u FROM User u WHERE YEAR(u.birthDate) >= :year")
+	public List<User> findByBirthDateStartingFromYear(@Param("year") int year);
 
 }
