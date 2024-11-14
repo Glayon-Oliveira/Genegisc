5aa078fb9392f69bc29f06265f602a6e0ec6c70c
Solansra <solansra@gmail.com>
2024-11-14 20:49:57 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/dto/UserSearchResultDTO.java
+++ src/main/java/com/lmlasmo/genegisc/dto/UserSearchResultDTO.java
@@ -1,5 +1,33 @@
 package com.lmlasmo.genegisc.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserSearchResultDTO {
 
+	@JsonProperty("sucess")
+	private boolean sucess = false;
+	
+	@JsonProperty("user")
+	private List<UserDTO> user;
+	
+	public UserSearchResultDTO() {}
+
+	public boolean isSucess() {
+		return sucess;
+	}
+
+	public void setSucess(boolean sucess) {
+		this.sucess = sucess;
+	}
+
+	public List<UserDTO> getUser() {
+		return user;
+	}
+
+	public void setUser(List<UserDTO> user) {
+		this.user = user;
+	}
+	
 }
