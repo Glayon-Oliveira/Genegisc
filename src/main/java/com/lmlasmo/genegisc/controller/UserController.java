e3ee9844b53bff619a4b95511818e190999cf8c6
Solansra <solansra@gmail.com>
2024-11-14 21:08:30 -0300

    Defined fields and methods

--- src/main/java/com/lmlasmo/genegisc/controller/UserController.java
+++ src/main/java/com/lmlasmo/genegisc/controller/UserController.java
@@ -1,5 +1,36 @@
 package com.lmlasmo.genegisc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lmlasmo.genegisc.dto.UserDTO;
+import com.lmlasmo.genegisc.dto.UserSearchResultDTO;
+import com.lmlasmo.genegisc.service.UserService;
+
+@RestController
 public class UserController {
+	
+	private UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/${username}")
+	public UserSearchResultDTO getUserByUsername(@RequestParam String username) {
+		
+		return userService.findByUsername(username);
+		
+	}
+	
+	@GetMapping("/profiles")
+	public UserSearchResultDTO findByProfileName(@RequestParam(name = "profile_name") String profilename) {
+		
+		return userService.findByProfileNameContaining(profilename);
+		
+	}
 
 }
